4e6812a650e9b31eb1a8754f72c8df3a596fbcd5
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-07-13 14:09:59 -0700

    added middleware for validing reqs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import config from "config";
 import connect from './utils/connect';
 import logger from './utils/logger';
+import routes from './routes/index';
 
 const port = config.get<number>(`port`);
 
@@ -13,4 +14,6 @@ app.listen(port, async () => {
     logger.info(`App is running at http://localhost:${port}`);
 
     await connect();
+
+    routes(app);
 });
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,9 @@
 
 const pino = require('pino');
 
+//We're using Pino to reduce resources for logging
+//Below, is how we're currently setting up our logger ie:Time format, and What to ignore.
+//Whenever we need to log from now on pull from utils directory.
 const logger = pino({
     transport: {
         target: 'pino-pretty',
