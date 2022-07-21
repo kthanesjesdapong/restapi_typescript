24b9912480816033f09a020c3404b90fa90ca7a0
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-07-21 15:24:08 -0700

    removed the s from user.models, built out skeletons for product services

---
+++ src/service/product.service.ts
@@ -0,0 +1,21 @@
+import { DocumentDefinition, FilterQuery } from "mongoose";
+import { omit } from 'lodash';
+import UserModel, { User } from '../models/user.model';
+import ProductModel, { Product } from '../models/product.model';
+
+
+export async function createProduct() {
+
+}
+
+export async function findProduct() {
+
+}
+
+export async function findAndUpdateProduct() {
+
+}
+
+export async function deleteProduct() {
+
+}
