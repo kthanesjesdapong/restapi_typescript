30100e3a82718a9cbcd409c6e6d46aa3458351d2
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-07-21 15:42:56 -0700

    Built out skeleton for Product Controller / Handlers

---
+++ src/controller/product.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+
+export async function createProductHandler(req: Request, res: Response) {
+
+}
+
+export async function updateProductHandler(req: Request, res: Response) {
+
+}
+
+export async function getProductHandler(req: Request, res: Response) {
+
+}
+
+export async function deleteProducthandler(req: Request, res: Response) {
+
+}
